a4b8c49ddd7372c2b20e5caa1ba6305a8db0c789
Tanmay Bhadauria <145855521+Tan28bha@users.noreply.github.com>
2025-10-27 20:59:36 +0530

    Practicing

--- 4.cpp
+++ 4.cpp
@@ -11,8 +11,8 @@ int removeduplicates(int arr[],int n){
    int i=0;
     for(int j=0;j<n;j++){
         if(arr[i]!=arr[j]){
+            arr[i+1]=arr[j];
             i++;
-            arr[i]=arr[j];
         }
     }
     return i+1;
--- 7.cpp
+++ 7.cpp
@@ -14,7 +14,7 @@ int search(int arr[],int n,int num){
 
 int main(){
     int arr[]={2,4,7,9,1,8,3,0};
-    int n =8;
+    int n =8;  
     int num =4;
     cout<<"The element num:"<<""<< num <<endl<<"found in index:"<<""<<search(arr,n,num);
 }
